bde4fd2eb680639270164e9e455bc715320b62cd
Alex Chorry <alex.chorry@gmail.com>
2015-12-04 01:06:30 +0300

    option to toggle markup state

--- glvrdPlugin/plugin.js
+++ glvrdPlugin/plugin.js
@@ -1,8 +1,5 @@
-var glvrdPlugin_url = 'http://api.glvrd.ru/v1/glvrd.js';
+var glvrdPlugin_url = 'https://api.glvrd.ru/v1/glvrd.js';
 
-if (!window.glvrd) {
-    $.getScript(glvrdPlugin_url, function () { console.log ('api is loaded' ) });
-}
 
 
 function getCaretCharacterOffsetWithin(element) {
@@ -19,7 +16,7 @@ function getCaretCharacterOffsetWithin(element) {
             preCaretRange.setEnd(range.endContainer, range.endOffset);
             caretOffset = preCaretRange.toString().length;
         }
-    } else if ( (sel = doc.selection) && sel.type != "Control") {
+    } else if ((sel = doc.selection) && sel.type != "Control") {
         var textRange = sel.createRange();
         var preCaretTextRange = doc.body.createTextRange();
         preCaretTextRange.moveToElementText(element);
@@ -40,80 +37,104 @@ var glvrdPlugin =
     icon: "glvrdPlugin",
     targetWnd: {
         "id": "glvrd_results",
-        "name" : "glvrd_name",
+        "name": "glvrd_name",
         "description": "glvrd_description"
     },
+    tagStart: function (r) {
+        return '<em class="glvrd-underline" data-rule="' + r + '">'
+    },
+    tagClose: '</em>',
+
     context_title: "glvrdPlugin",
+    markupState: true, // 0 - next call will add markup, 1 - next call will remove it
     exec: function (editor) {
         this.editor = editor;
         this.ruleset = {};
         var data = editor.getSnapshot();
         editor.element.data("text", data);
 
-        glvrd.getStatus(function(r) {
-            if ( r!== undefined && r.status =='ok')
-            {
-                result = glvrdPlugin.proofRead(data);
+        glvrd.getStatus(function (r) {
+            if (r !== undefined && r.status == 'ok') {
+                if (glvrdPlugin.markupState) {
+                    glvrdPlugin.proofRead(data);
+                } else {
+                    glvrdPlugin.setText(glvrdPlugin.removeGlvrdMarkup(data));
+                }
+                glvrdPlugin.markupState = !glvrdPlugin.markupState;
+            } else {
+                var msg = '';
+                if (r !== undefined && r.hasOwnProperty('status')) {
+                    msg = '\nStatus is ' + r.status + ", msg: " + r.message + "(" + r.code +")";
+                }
+                alert('Sorry, glvr API is currently unavalable :( ' + msg);
+                //TODO: alert sucks, gotta use something more pretty
             }
         });
     },
 
-    setText: function(text) {
+    addGlvrdMarkup: function (text, v, offset, ruleName) {
+        var t = text.substring(0, v.start + offset)
+            + glvrdPlugin.tagStart(ruleName) + text.substring(v.start + offset, v.end + offset)
+            + glvrdPlugin.tagClose + text.substring(v.end + offset, text.length);
+        return t;
+    },
+
+    removeGlvrdMarkup: function (data) {
+        console.debug(data);
+        var t=glvrdPlugin.stripRuleTags(data);
+        console.debug(t);
+        return t;
+    },
+
+    setText: function (text) {
         this.editor.loadSnapshot(text);
     },
 
-    stripRuleTags: function(text)
-    {
-        var reg = /(<em[^>]*data-rule="r\d+"[^>]*>).+?(<\/em>)/g;
-        return text.replace(reg,'');
+    stripRuleTags: function (data) {
+        data = data.replace(/(\<em)([ a-zA-Z\=\"\-0-9]+(\>))/g, "");
+        data = data.replace(/(\<\/em\>)/g, "");
+        return data;
     },
 
-    proofRead: function(data) {
-
+    proofRead: function (data) {
         window.glvrd.proofread(
-            glvrdPlugin.stripRuleTags( data ),
+            glvrdPlugin.stripRuleTags(data),
             function (result) {
-            var offset = 0;
-            $.each(result.fragments, function (k, v) {
-                var ruleName = 'r' + k;
-                var tagStart = '<em class="glvrd-underline" data-rule="' + ruleName + '">';
-                var tagClose = '</em>';
-
-                var offsetLen = tagStart.length + tagClose.length;
-
-                data = data.substring(0,v.start+offset)
-                    + tagStart + data.substring(v.start+offset, v.end+offset)
-                    + tagClose + data.substring(v.end+offset, data.length);
-                offset += offsetLen;
-                glvrdPlugin.ruleset[ruleName] = v.hint;
+                var offset = 0;
+                $.each(result.fragments, function (k, v) {
+                    var ruleName = 'r' + k;
+                    data = glvrdPlugin.addGlvrdMarkup(data, v, offset, ruleName);
+                    offset += glvrdPlugin.tagStart(ruleName).length + glvrdPlugin.tagClose.length;
+                    glvrdPlugin.ruleset[ruleName] = v.hint;
+                });
+                glvrdPlugin.setText(data);
+                glvrdPlugin.registerHover(glvrdPlugin.ruleset);
             });
-            glvrdPlugin.setText(data);
-            glvrdPlugin.registerHover(glvrdPlugin.ruleset);
-        });
     },
 
-    registerHover: function(ruleset) {
+    registerHover: function (ruleset) {
         var ckTextFrameName = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].id + '_contents iframe';
         var target = $('#' + ckTextFrameName).contents();
-        $.each(ruleset, function(k,v){
-            var emTarget = target.find('em').filter('[data-rule="'+ k + '"]');
-            emTarget.on('mouseenter', function(e){
-                $('#'+glvrdPlugin.targetWnd.name).html( ruleset[k].name );
-                $('#'+glvrdPlugin.targetWnd.description).html( ruleset[k].description );
+        $.each(ruleset, function (k, v) {
+            var emTarget = target.find('em').filter('[data-rule="' + k + '"]');
+            emTarget.on('mouseenter', function (e) {
+                $('#' + glvrdPlugin.targetWnd.name).html(ruleset[k].name);
+                $('#' + glvrdPlugin.targetWnd.description).html(ruleset[k].description);
                 $(this).addClass('glvrd-underline-active');
-            }).on('mouseleave', function(e) {
+            }).on('mouseleave', function (e) {
                 $(this).removeClass('glvrd-underline-active');
             });
-            glvrdPlugin.trackChanges(emTarget,v);
+            //glvrdPlugin.trackChanges(emTarget,v);
         });
     },
-    trackChanges: function(target,ruleItem) {
-        target.on('DOMSubtreeModified', function(e){
-                //console.debug( this.innerHTML );
+
+    trackChanges: function (target, ruleItem) {
+        target.on('DOMSubtreeModified', function (e) {
         });
     },
+
     // todo: fire partial text update if no changes has been made to specified target within X seconds
-    textTimeUpdate: function(time, position) {
+    textTimeUpdate: function (time, position) {
 
     },
     inlineHints: function (target, data) {
@@ -126,12 +147,10 @@ CKEDITOR.plugins.add('glvrdPlugin', {
     icons: 'glvrdPlugin',
     init: function (editor) {
         console.log('glvrd loaded');
-        editor.addContentsCss( CKEDITOR.plugins.getPath( 'glvrdPlugin' ) + 'styles/glvrd.css' );
+        editor.addContentsCss(CKEDITOR.plugins.getPath('glvrdPlugin') + 'styles/glvrd.css');
         editor.addCommand('glvrdPlugin', {
             exec: function (editor) {
                 glvrdPlugin.exec(editor);
-                //var now = new Date();
-                //editor.insertHtml('The current date and time is: <em>' + now.toString() + '</em>');
             }
         });
         editor.ui.addButton(glvrdPlugin.name, {
